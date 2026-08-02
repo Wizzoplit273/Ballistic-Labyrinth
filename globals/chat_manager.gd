040d261eb3835b61c134eef3a0c0275d892ef580
Wizzoplit273 <88540529+Wizzoplit273@users.noreply.github.com>
2026-08-02 14:14:32 +0300

    System chat messages snapshot

--- globals/chat_manager.gd
+++ globals/chat_manager.gd
@@ -3,6 +3,9 @@ extends Node
 var chat_history: Array[Dictionary] = []
 var MAX_HISTORY: int = 100
 
+func send_local_message(text: String, channel: String = "global") -> void:
+	process_message(text, channel)
+
 func send_message(text: String, channel: String = "global") -> void:
 	if NetworkManager.is_online and not multiplayer.is_server():
 		rpc_id(1, "process_message", text, channel)
@@ -20,11 +23,28 @@ func update_chat_history(new_message: Dictionary) -> void:
 signal update_local_chat_ui(messages: Array[Dictionary])
 @rpc("any_peer", "reliable", "call_local")
 func process_message(text: String, channel: String = "global") -> void:
-	if NetworkManager.is_online and not multiplayer.is_server(): return
 	var sender_id: int = multiplayer.get_remote_sender_id()
-	if sender_id == 0 and NetworkManager.is_online: sender_id = 1
 	var final_text: String = text.strip_edges()
-	var message: Dictionary = {
+	var message: Dictionary = {}
+	var new_array: Array[Dictionary] = []
+	if NetworkManager.is_online and not multiplayer.is_server():
+		if sender_id != 0: return
+		message = {
+			"sender_sid": sender_id,
+			"sender_name": SessionManager.profile_data.get("name"),
+			"text": final_text,
+			"timestamp": Time.get_time_string_from_unix_time(int(Time.get_unix_time_from_system())),
+			"channel": channel
+		}
+		chat_history.append(message)
+		while chat_history.size() > MAX_HISTORY:
+			chat_history.pop_front()
+		new_array.append(message)
+		update_local_chat_ui.emit(new_array)
+		return
+	if sender_id == 0 and NetworkManager.is_online: sender_id = 1
+	if sender_id > 1: channel = "global"
+	message = {
 		"sender_sid": sender_id,
 		"sender_name": SessionManager.data.get(sender_id).get("name"),
 		"text": final_text,
@@ -35,5 +55,5 @@ func process_message(text: String, channel: String = "global") -> void:
 	while chat_history.size() > MAX_HISTORY:
 		chat_history.pop_front()
 	if NetworkManager.is_online: rpc("update_chat_history", message)
-	var new_array: Array[Dictionary] = [message]
+	new_array.append(message)
 	update_local_chat_ui.emit(new_array)
