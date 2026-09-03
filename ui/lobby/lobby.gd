69bc84bc350bce9fe3a92e0bd38909c40e4b8c2d
Wizzoplit273 <88540529+Wizzoplit273@users.noreply.github.com>
2026-09-03 14:51:56 +0300

    Removed join button: opting for /join {ws/wss url} instead

--- ui/lobby/lobby.gd
+++ ui/lobby/lobby.gd
@@ -92,9 +92,6 @@ func _on_discard_button_pressed() -> void:
 	unfocus()
 	$ExitConfirmDialog.visible = false
 
-func _on_join_game_button_pressed() -> void:
-	NetworkManager.start_client()
-
 func _on_exit_game_button_pressed() -> void:
 	unfocus()
 	$ExitConfirmDialog.visible = true
@@ -107,6 +104,3 @@ func _on_username_edit_text_submitted(new_text: String) -> void:
 func _on_player_color_picker_color_changed(color: Color) -> void:
 	%PlayerColorTest.modulate = color
 	SessionManager.set_profile_color(color)
-
-func _on_url_edit_text_changed(new_text: String) -> void:
-	pass # Replace with function body.
